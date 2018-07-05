45cd56f8abb16835e7fb9c83e52a9ff892606781
Andrew Jiao <ayj5cj@virginia.edu>
2018-07-05 17:56:59 -0400

    3rd order Runge-Kutta, "attempted" WENO

--- mhd3.py
+++ mhd3.py
@@ -4,12 +4,15 @@ from scipy.sparse import dia_matrix
 from scipy.linalg import eig
 from scipy.sparse.linalg import eigs
 
-def grid(size=10, max=1):
-    nx = 2 + size
+def grid(size=10, max=1, ghost=1):
+    global nx, dx, x, xx, gh
+    nx = 2 * gh + size
     dx = max/size
-    x = np.linspace(-dx/2, max + dx/2, nx)
+    x = np.linspace(-(2*gh - 1)/2 * dx, max + (2*gh - 1)/2 * dx, nx)
     xx = np.reshape(x, (nx, 1))
-    return nx, max, dx, x, xx
+    gh = ghost
+    
+    return nx, dx, x, xx
 
 
 def FD_matrix(nr, dr, order):
@@ -26,7 +29,7 @@ def FD_matrix(nr, dr, order):
     return dv
     
 
-def equilibrium(FD_matrix, zero_out, r, rr, nr, dr):
+def equilibrium(FD_matrix, zero_out):
     P = np.exp(-rr**4) + 0.05
     op = zero_out(FD_matrix(nr, dr, 1) / 2 + dia_matrix((1.0 / r, 0), shape=(nr, nr)).toarray())
     
@@ -144,13 +147,13 @@ def create_M(rr, nr, dr, rho_0, B_0, FD_matrix, k, zero_out, BC, DV_product):
     # Electron pressure term
     m_Btheta_rho = m_Btheta_rho + D_P * zero_out(np.diagflat(k * (1 / rho_0**2 * (FD_matrix(nr, dr, 1) @ rho_0) + FD_matrix(nr, dr, 1) @ (1 / rho_0)), 0))
     
-    # BOUNDARY CONDITIONS 
+    # Boundary conditions
     m_rho_rho = BC(m_rho_rho, nr, 1, 0)
     m_Br_Br = BC(m_Br_Br, nr, 0, 0)
     m_Btheta_Btheta = BC(m_Btheta_Btheta, nr, 0, 0)
     m_Bz_Bz = BC(m_Bz_Bz, nr, 0, 0)
     m_Vr_Vr = BC(m_Vr_Vr, nr, 0, 1)
-    m_Vtheta_Vtheta = BC(m_Vtheta_Vtheta, nr, 0, 0)
+    m_Vtheta_Vtheta = BC(m_Vtheta_Vtheta, nr, 0, 1)
     m_Vz_Vz = BC(m_Vz_Vz, nr, 1, 1)
     
     M = np.block([[m_rho_rho, m0, m0, m0, m_rho_Vr, m_rho_Vtheta, m_rho_Vz], 
@@ -161,10 +164,6 @@ def create_M(rr, nr, dr, rho_0, B_0, FD_matrix, k, zero_out, BC, DV_product):
 				[m_Vtheta_rho, m0, m0, m0, m0, m_Vtheta_Vtheta, m0],
 				[m_Vz_rho, m0, m_Vz_Btheta, m_Vz_Bz, m0, m0, m_Vz_Vz]])
 
-#     M = np.block([[m_rho_rho, m0, m_rho_Vr, m_rho_Vz], 
-# 				[m_Btheta_rho, m_Btheta_Btheta, m_Btheta_Vr, m_Btheta_Vz], 
-# 				[m_Vr_rho, m_Vr_Btheta, m_Vr_Vr, m0],
-# 				[m_Vz_rho, m_Vz_Btheta, m0, m_Vz_Vz]])
     return M
 
 
@@ -208,21 +207,107 @@ def convergence(grid, res, FD_matrix, equilibrium, zero_out, BC, DV_product, cre
     plt.show()
 
 
+# def L(WENO, f, fp, fm, phi_N, u):
+#     udv2 = np.reshape(np.zeros(nr), (nr, 1))
+#     udv2[1: -1] = 1 / dr**2 * (u[0: -2] - 2 * u[1: -1] + u[2: ])
+#     return WENO(f, fp, fm, phi_N, u)
+
+
+def runge_kutta(WENO, f, fp, fm, phi_N, u, dt):
+    u1 = np.reshape(np.zeros(nr), (nr, 1))
+    u2 = np.reshape(np.zeros(nr), (nr, 1))
+    u3 = np.reshape(np.zeros(nr), (nr, 1))
+    u1 = u + dt * WENO(f, fp, fm, phi_N, u)
+    u2 = 3/4 * u + 1/4 * u1 + 1/4 * dt * WENO(f, fp, fm, phi_N, u1)
+    u3 = 1/3 * u + 2/3 * u2 + 2/3 * dt * WENO(f, fp, fm, phi_N, u2)
+    
+    return u3
+
+
+# Testing inviscid 1D Burger's equation
+def test(B_0, t_max, WENO, f, fp, fm, phi_N):
+    B = B_0
+    dt = dr**2 / 2
+    t = 0
+    while t < t_max:
+        t = t + dt
+        B_temp = B.copy()
+        B = runge_kutta(WENO, f, fp, fm, phi_N, B_temp, dt)
+        B[0] = 0
+        B[-1] = B[-2] * r[-2] / r[-1]
+        
+    plt.plot(r[1: -1], B[1: -1])
+    plt.legend(['B'])
+    plt.xlabel('r/r0')
+    plt.title('Time evolution')
+    plt.show()      
+
+
+def phi_N(a, b, c, d):
+    # Small parameter to avoid division by 0 in Weights
+    epsilon = 1e-6
+    
+    # Smoothness indicators
+    IS0 = 13 * (a - b)**2 + 3 * (a - 3*b)**2
+    IS1 = 13 * (b - c)**2 + 3 * (b + c)**2
+    IS2 = 13 * (c - d)**2 + 3 * (3*c - d)**2
+    
+    # Weights
+    alpha0 = 1 / (epsilon + IS0)**2
+    alpha1 = 6 / (epsilon + IS1)**2
+    alpha2 = 3 / (epsilon + IS2)**2
+    omega0 = alpha0 / (alpha0 + alpha1 + alpha2)
+    omega2 = alpha2 / (alpha0 + alpha1 + alpha2)
+    
+    return 1/3 * omega0 * (a - 2*b + c) + 1/6 * (omega2 - 1/2) * (b - 2*c + d)
+    
+    
+def f(u):
+    return 1/2 * u**2
+    
+    
+def fp(f, u):
+    alpha = max(u)
+    return 1/2 * (f(u) + alpha * u)
+def fm(f, u):
+    alpha = max(u)
+    return 1/2 * (f(u) - alpha * u)    
+ 
+   
+def WENO(f, fp, fm, phi_N, u):
+    
+    # CHECK NEGATIVE FLUX COMPONENTS
+    fhat_right = np.reshape(np.zeros(nr), (nr, 1))
+    fhat_right[3: -3] = (1/12 * (-f(u[2: -4]) + 7 * f(u[3: -3]) + 7 * f(u[4: -2]) - f(u[5: -1]))
+                  - phi_N(fp(u[2: -4]) - fp(u[1: -5]), fp(u[3: -3]) - fp(u[2: -4]), fp(u[4: -2]) - fp(u[3: -3]), fp(u[5: -1]) - fp(u[4: -2]))
+                  + phi_N(fm(u[6:   ]) - fm(u[5: -1]), fm(u[5: -1]) - fm(u[4: -2]), fm(u[4: -2]) - fm(u[3: -3]), fm(u[3: -3]) - fm(u[2: -4])))
+    
+    fhat_left  = np.reshape(np.zeros(nr), (nr, 1))
+    fhat_left[3: -3]  = (1/12 * (-f(u[1: -5]) + 7 * f(u[2: -4]) + 7 * f(u[3: -3]) - f(u[4: -2]))
+                  - phi_N(fp(u[1: -5]) - fp(u[0: -6]), fp(u[2: -4]) - fp(u[1: -5]), fp(u[3: -3]) - fp(u[2: -4]), fp(u[4: -2]) - fp(u[3: -3]))
+                  + phi_N(fm(u[5: -1]) - fm(u[4: -2]), fm(u[4: -2]) - fm(u[3: -3]), fm(u[3: -3]) - fm(u[2: -4]), fm(u[2: -4]) - fm(u[1: -5])))
+    
+    df_dr = np.reshape(np.zeros(nr), (nr, 1))
+    df_dr[3: -3] = 1/dr * (fhat_right[3: -3] - fhat_left[3: -3])
+    
+    return df_dr
+
+
 def pinch(rho, B, rr, nr, dr, t_max):
 #     nt = int(t_max / dt)
     t = 0
     Vr = np.reshape(np.zeros(nr), (nr, 1))
-    dt = 0.00005
+#     dt = 0.000001
   
     while t < t_max:
         rho_temp = rho.copy()
         B_temp = B.copy()
         Vr_temp = Vr.copy()       
 
-#         v_A = max(B / np.sqrt(4 * np.pi * np.abs(rho)))
-#         dt = dr / (20 * v_A)
+        v_A = max(B / np.sqrt(4 * np.pi * np.abs(rho)))
+        dt = dr / (15 * v_A)
         t = t + dt
-#         print(v_A, t)
+        print(v_A, t)
                 
         rho[1: -1] = rho_temp[1: -1] - dt / (rr[1: -1] * 2 * dr) * (rr[2: ] * rho_temp[2: ] * Vr_temp[2: ] - rr[: -2] * rho_temp[: -2] * Vr_temp[: -2])
         B[1: -1] = (B_temp[1: -1] - dt / (2 * dr) * (Vr_temp[2: ] * B_temp[2: ] - Vr_temp[: -2] * B_temp[: -2]) 
@@ -239,6 +324,8 @@ def pinch(rho, B, rr, nr, dr, t_max):
 
     plt.plot(r[1: -1], B[1: -1], r[1: -1], rho[1: -1], r[1: -1], Vr[1: -1])
     plt.legend(['B', 'rho', 'V_r'])
+    plt.xlabel('r/r0')
+    plt.title('Time evolution')
     plt.show()      
 
 def plot_eigenvalues(M, G):
@@ -270,7 +357,7 @@ def plot_mode(i):
     V_r = v_omega[4*nr: 5*nr]
     V_theta = v_omega[5*nr: 6*nr]
     V_z = v_omega[6*nr: 7*nr]
-    phase = np.exp(-1j * np.angle(rho[2]))
+    phase = np.exp(-1j * np.angle(rho[0]))
 #     print(f1(phase * B_theta)[0:10])
 #     print(f1(phase * B_theta)[-10:])
 #   
@@ -381,44 +468,46 @@ def plot_mode(i):
     plt.show()
 
 
-##
-nr, r_max, dr, r, rr = grid(size=800, max=5.0)
-rho_0, B_0, J_0 = equilibrium(FD_matrix, zero_out, r, rr, nr, dr)
+## GRID SIZE, EQUILIBRIUM
+nr, dr, r, rr = grid(size=400, max=5.0, ghost=1)
+rho_0, B_0, J_0 = equilibrium(FD_matrix, zero_out)
 
-# plt.plot(r[1: -1], B_0[1: -1], r[1: -1], rho_0[1: -1], r[1: -1], J_0[1: -1])
+# plt.plot(r[gh: -gh], B_0[gh: -gh], r[gh: -gh], rho_0[gh: -gh], r[gh: -gh], J_0[gh: -gh])
 # plt.show()
 
-k = 1
-m = 0
-D_eta = 0.2
-D_H = 1
-D_P = 0
-nz, z_max, dz, z, zz = grid(size=200, max=2*np.pi/k)
-z_osc = np.exp(1j * k * zz)
-G = create_G(nr)
+# test(B_0, t_max, WENO, f, fp, fm, phi_N)
 
-##
-B_1 = 1.5 * B_0.copy()
-pinch(rho_0, B_1, rr, nr, dr, 0.4)
+## PARAMETERS
+# k = 1
+# m = 0
+# D_eta = 1e-4
+# D_H = 0.3
+# D_P = 0
+# nz, dz, z, zz = grid(size=400, max=2*np.pi/k)
+# z_osc = np.exp(1j * k * zz)
+# G = create_G(nr)
 
-##
-res_min = 20
-res_max = 200
-d_res = 10
-n_res = 1 + (res_max - res_min)/d_res
-res = np.linspace(res_min, res_max, n_res)
+## TIME EVOLUTION
+# B_1 = 2 * B_0.copy()
+# pinch(rho_0, B_1, rr, nr, dr, 0.3)
 
-# k = 1 for convergence
+## CONVERGENCE TEST
+# res_min = 20
+# res_max = 200
+# d_res = 10
+# n_res = 1 + (res_max - res_min)/d_res
+# res = np.linspace(res_min, res_max, n_res)
 # convergence(grid, res, FD_matrix, equilibrium, zero_out, BC, DV_product, create_G)
 
-k_min = 0.5
-k_max = 4
-dk = 0.5
-nk = 1 + (k_max - k_min)/dk
-kk = np.linspace(k_min, k_max, nk)
-
+## DISPERSION
+# k_min = 0.5
+# k_max = 10
+# dk = 0.5
+# nk = 1 + (k_max - k_min)/dk
+# kk = np.linspace(k_min, k_max, nk)
 # gamma_vs_k(G, rr, nr, dr, rho_0, B_0, FD_matrix, kk, zero_out, BC, DV_product, create_M)
 
+## EIGENSYSTEM
 # M = create_M(rr, nr, dr, rho_0, B_0, FD_matrix, k, zero_out, BC, DV_product)
 # plot_eigenvalues(M, G)
 # plot_mode(1)
